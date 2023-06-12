9a1863915272ae0ff321789be98b4ba86b103649
YuSetiawan <ysfstiawan@gmail.com>
2023-06-12 11:10:42 +0700

    add divider per-item

--- Number3GET.js
+++ Number3GET.js
@@ -1,47 +1,38 @@
 // Web API 1
-console.log('=== aplikasi quotes dari dummy json ===');
-const axios = require('axios');
-axios
-  .get('https://dummyjson.com/quotes')
-  // berhasil GET
-  .then((results) => {
-    let data = results.data.quotes;
-    let quote = data.map((item) => {
-      return item.quote;
-    });
-    let author = data.map((item) => {
-      return item.author;
-    });
-    console.log(`name: ${author}`);
-    console.log(`quote: ${quote}`);
-  })
-  // gagal GET
-  .catch(function (error) {
-    console.log(error);
-  });
-
-// Web API 2
-// console.log('=== aplikasi products dari dummy json ===');
+// console.log('=== aplikasi quotes dari dummy json ===');
 // const axios = require('axios');
 // axios
-//   .get('https://dummyjson.com/products')
+//   .get('https://dummyjson.com/quotes')
 //   // berhasil GET
-//   .then((res) => {
-//     let data = res.data.products;
-//     let title = data.map((item) => {
-//       return item.title;
+//   .then((results) => {
+//     let data = results.data.quotes;
+//     data.map((item) => {
+//       console.log(`quote: ${item.quote}`);
+//       console.log(`author: ${item.author}`);
+//       console.log(`======================`);
 //     });
-//     let category = data.map((item) => {
-//       return item.category;
-//     });
-//     let price = data.map((item) => {
-//       return item.price;
-//     });
-//     console.log(`product name: ${title}`);
-//     console.log(`category: ${category}`);
-//     console.log(`price: ${price}`);
 //   })
 //   // gagal GET
 //   .catch(function (error) {
 //     console.log(error);
 //   });
+
+// Web API 2
+console.log('=== aplikasi products dari dummy json ===');
+const axios = require('axios');
+axios
+  .get('https://dummyjson.com/products')
+  // berhasil GET
+  .then((res) => {
+    let data = res.data.products;
+    data.map((item) => {
+      console.log(`product name: ${item.title}`);
+      console.log(`category: ${item.category}`);
+      console.log(`price: ${item.price}`);
+      console.log(`======================`);
+    });
+  })
+  // gagal GET
+  .catch(function (error) {
+    console.log(error);
+  });
